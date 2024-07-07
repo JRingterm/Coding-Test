f7f85ef6a7ecff3fd14f65db4734ee7ec9afecb8
JRingterm <ghwns3712@naver.com>
2024-07-07 21:27:18 +0900

    [Feat] 2738 풀이, 행렬덧셈 설명 추가, *의 Unpacking 기능 설명 추가

---
+++ 파이썬/2차원 배열/2738.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+A = []
+B = []
+
+for i in range(N):
+    A.append(list(map(int, input().split())))
+for i in range(N):
+    B.append(list(map(int, input().split())))
+
+for i in range(N):
+    C = []
+    for j in range(M):
+        C.append(A[i][j] + B[i][j])
+    print(*C)
