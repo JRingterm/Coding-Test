24faef16abce05202955177ef93f74002828e214
JRingterm <ghwns3712@naver.com>
2024-08-12 17:25:32 +0900

    [feat] 2501, 5086 풀이

---
+++ 파이썬/약수, 배수와 소수/2501.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+measure = []
+
+for i in range(1, N+1):
+    if N % i == 0:
+        measure.append(i)
+
+if len(measure) >= K:
+    print(measure[K-1])
+else:
+    print(0)
